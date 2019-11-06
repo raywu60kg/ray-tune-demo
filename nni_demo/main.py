f477ac639e765b121f690b04f4aaf3bc90a7a220
RayWu <rytesfabc@gmail.com>
2019-11-06 16:10:10 +0800

    update main

---
+++ nni_demo/main.py
@@ -0,0 +1,11 @@
+from sklearn.datasets import load_iris
+from sklearn.linear_model import LogisticRegression
+
+X, y = load_iris(return_X_y=True)
+clf = LogisticRegression(random_state=0, solver='lbfgs',
+                         multi_class='multinomial').fit(X, y)
+clf.score(X, y)
+
+
+if __name__ == '__main__':
+    print(clf.score(X, y))
